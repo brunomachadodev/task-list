0b7fe4c650726b18ae18ff3fbf39f4668371bb36
Bruno Machado <bruno-mla@hotmail.com>
2021-08-01 17:14:34 -0300

    Finished

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,6 +16,7 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
 
     let newTask = {
       id: Math.floor(Math.random() * 10000),
@@ -24,14 +25,29 @@ export function TaskList() {
     };
 
     setTasks([...tasks, newTask]);
+    setNewTaskTitle("");
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    const newTasks = tasks.map((task) =>
+      task.id === id
+        ? {
+            ...task,
+            isComplete: !task.isComplete,
+          }
+        : task
+    );
+
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter((task) => task.id !== id);
+
+    setTasks(filteredTasks);
   }
 
   return (
@@ -73,7 +89,7 @@ export function TaskList() {
                   />
                   <span className="checkmark"></span>
                 </label>
-                <p>{task.title ?? ""}</p>
+                <p>{task.title}</p>
               </div>
 
               <button
